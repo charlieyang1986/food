6dce8ede616d0e7b130ca2806d17ee5cee0f6431
Chunhai Yang <cyang21@mail.greenriver.edu>
2020-04-29 15:49:29 -0400

    index page and composer added

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+// Turn on error reporting
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+// require the autoload file
+
+require_once("vendor/autoload.php");
+
+// Instantiate the F3 base class
+$f3 = Base::instance();
+
+//default route
+$f3->route('GET /',function(){
+  // echo '<h1>Welcome to my food page!</h1>';
+
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+
+
+
+//Run F3
+$f3->run();
+
